a01c4f68d46af1367d34c4c0f246c9a413404aae
SaniaGos <saniagoss@gmail.com>
2021-04-28 12:49:32 +0300

    new4.0

--- CatchMe/Form1.Designer.cs
+++ CatchMe/Form1.Designer.cs
@@ -30,32 +30,25 @@ namespace CatchMe
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.buttonMouse = new System.Windows.Forms.Button();
-            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.StripMenu = new System.Windows.Forms.ToolStripMenuItem();
-            this.timer = new System.Windows.Forms.Timer(this.components);
-            this.contextMenuStrip1.SuspendLayout();
+            this.timerFreeze = new System.Windows.Forms.Timer(this.components);
+            this.timerTime = new System.Windows.Forms.Timer(this.components);
+            this.buttonMouse = new System.Windows.Forms.Button();
+            this.panel = new System.Windows.Forms.Panel();
+            this.labelPoint = new System.Windows.Forms.Label();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.contextMenuStrip.SuspendLayout();
+            this.panel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // buttonMouse
+            // contextMenuStrip
             // 
-            this.buttonMouse.BackgroundImage = global::CatchMe.Properties.Resources.jerry;
-            this.buttonMouse.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.buttonMouse.Location = new System.Drawing.Point(355, 222);
-            this.buttonMouse.Name = "buttonMouse";
-            this.buttonMouse.Size = new System.Drawing.Size(40, 40);
-            this.buttonMouse.TabIndex = 0;
-            this.buttonMouse.UseVisualStyleBackColor = true;
-            this.buttonMouse.Click += new System.EventHandler(this.button1_Click);
-            this.buttonMouse.MouseMove += new System.Windows.Forms.MouseEventHandler(this.buttonMouse_MouseMove);
-            // 
-            // contextMenuStrip1
-            // 
-            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.StripMenu});
-            this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(255, 48);
-            this.contextMenuStrip1.MouseLeave += new System.EventHandler(this.contextMenuStrip1_MouseLeave);
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(255, 26);
+            this.contextMenuStrip.MouseLeave += new System.EventHandler(this.contextMenuStrip1_MouseLeave);
             // 
             // StripMenu
             // 
@@ -64,38 +57,90 @@ namespace CatchMe
             this.StripMenu.Text = "Заморозити мишу на чверть сек";
             this.StripMenu.Click += new System.EventHandler(this.StripMenu_Click);
             // 
-            // timer
+            // timerFreeze
             // 
-            this.timer.Interval = 250;
-            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            this.timerFreeze.Interval = 250;
+            this.timerFreeze.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // timerTime
+            // 
+            this.timerTime.Enabled = true;
+            this.timerTime.Tick += new System.EventHandler(this.timerTime_Tick);
+            // 
+            // buttonMouse
+            // 
+            this.buttonMouse.BackgroundImage = global::CatchMe.Properties.Resources.jerry;
+            this.buttonMouse.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonMouse.Location = new System.Drawing.Point(363, 240);
+            this.buttonMouse.Name = "buttonMouse";
+            this.buttonMouse.Size = new System.Drawing.Size(40, 40);
+            this.buttonMouse.TabIndex = 0;
+            this.buttonMouse.UseVisualStyleBackColor = true;
+            this.buttonMouse.Click += new System.EventHandler(this.buttonMouse_Click);
+            this.buttonMouse.MouseMove += new System.Windows.Forms.MouseEventHandler(this.buttonMouse_MouseMove);
+            // 
+            // panel
+            // 
+            this.panel.BackgroundImage = global::CatchMe.Properties.Resources.tom;
+            this.panel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel.Controls.Add(this.buttonMouse);
+            this.panel.Location = new System.Drawing.Point(1, 35);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(780, 525);
+            this.panel.TabIndex = 2;
+            this.panel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel_MouseDown);
+            this.panel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel_MouseMove);
+            // 
+            // labelPoint
+            // 
+            this.labelPoint.Font = new System.Drawing.Font("Monotype Corsiva", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelPoint.Location = new System.Drawing.Point(1, 3);
+            this.labelPoint.Name = "labelPoint";
+            this.labelPoint.Size = new System.Drawing.Size(125, 30);
+            this.labelPoint.TabIndex = 3;
+            this.labelPoint.Text = "Point:  0";
+            this.labelPoint.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelTime
+            // 
+            this.labelTime.Font = new System.Drawing.Font("Monotype Corsiva", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelTime.Location = new System.Drawing.Point(656, 3);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(125, 30);
+            this.labelTime.TabIndex = 3;
+            this.labelTime.Text = "Time";
+            this.labelTime.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // Form1
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.BackgroundImage = global::CatchMe.Properties.Resources.tom;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(784, 462);
-            this.Controls.Add(this.buttonMouse);
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.labelTime);
+            this.Controls.Add(this.labelPoint);
+            this.Controls.Add(this.panel);
             this.Cursor = System.Windows.Forms.Cursors.Hand;
             this.DoubleBuffered = true;
-            this.MaximumSize = new System.Drawing.Size(800, 500);
-            this.MinimumSize = new System.Drawing.Size(800, 500);
+            this.MaximumSize = new System.Drawing.Size(800, 600);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Tom and Jerry";
-            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
-            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
-            this.contextMenuStrip1.ResumeLayout(false);
+            this.contextMenuStrip.ResumeLayout(false);
+            this.panel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button buttonMouse;
-        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
         private System.Windows.Forms.ToolStripMenuItem StripMenu;
-        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Timer timerFreeze;
+        private System.Windows.Forms.Timer timerTime;
+        private System.Windows.Forms.Button buttonMouse;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Label labelPoint;
+        private System.Windows.Forms.Label labelTime;
     }
 }
 
--- CatchMe/Form1.cs
+++ CatchMe/Form1.cs
@@ -12,92 +12,25 @@ namespace CatchMe
 {
     public partial class Form1 : Form
     {
+        private int point;
+        private int time;
         public Form1()
         {
             InitializeComponent();
+            point = 0;
+            time = 0;
         }
 
-        private void Form1_MouseMove(object sender, MouseEventArgs e)
-        {
-            Point tmp = new Point(buttonMouse.Location.X + 20, buttonMouse.Location.Y + 20);
-            int distance = Distanse(tmp, e.Location);
-            if (distance < 50)
-            {
-                tmp.X += 10;
-                if (tmp.X <= this.Width - 45 && Distanse(tmp, e.Location) >= 50)
-                {
-                    buttonMouse.Location = new Point(tmp.X - 20, tmp.Y - 20);
-                    return;
-                }
-                tmp.X -= 20;
-                if (tmp.X >= 30 && Distanse(tmp, e.Location) >= 50)
-                {
-                    buttonMouse.Location = new Point(tmp.X - 20, tmp.Y - 20);
-                    return;
-                }
-                tmp.X += 10;
-                tmp.Y += 10;
-                if (tmp.Y <= this.Height - 55 && Distanse(tmp, e.Location) >= 50)
-                {
-                    buttonMouse.Location = new Point(tmp.X - 20, tmp.Y - 20);
-                    return;
-                }
-                tmp.Y -= 20;
-                if (tmp.Y >= 30 && Distanse(tmp, e.Location) >= 50)
-                {
-                    buttonMouse.Location = new Point(tmp.X - 20, tmp.Y - 20);
-                    return;
-                }
-            }
-            if (distance < 50 && IsHouse())
-            {
-                ChooseDoor();
-            }
-        }
-        private void ChooseDoor()
-        {
-            int door = MyRand.GetRand(1, 4);
-            switch (door)
-            {
-                case 1:
-                    buttonMouse.Location = new Point(20, 20);
-                    break;
-                case 2:
-                    buttonMouse.Location = new Point(Width - 80, 20);
-                    break;
-                case 3:
-                    buttonMouse.Location = new Point(20, Height - 90);
-                    break;
-                case 4:
-                    buttonMouse.Location = new Point(Width - 80, Height - 90);
-                    break;
-                default:
-                    break;
-            }
-        }
-        private bool IsHouse()
-        {
-            if (buttonMouse.Location.X < 40 && buttonMouse.Location.Y < 40)
-                return true;
-            if (buttonMouse.Location.X + 40 > Width - 50 && buttonMouse.Location.Y < 40)
-                return true;
-            if (buttonMouse.Location.X < 40 && buttonMouse.Location.Y + 40 > Height - 70)
-                return true;
-            if (buttonMouse.Location.X + 40 > Width - 50 && buttonMouse.Location.Y + 40 > Height - 70)
-                return true;
-            return false;
-
-        }
-        private int Distanse(Point a, Point b)
-        {
-            return (int)Math.Sqrt(Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y - b.Y, 2));
-        }
-        private void button1_Click(object sender, EventArgs e)
+        //
+        // Mouse
+        //
+        private void buttonMouse_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Вспіймали", "Яй!!!", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            buttonMouse.Location = new Point(Width / 2 - 20, Height / 2 - 20);
+            buttonMouse.Location = new Point(panel.Width / 2 - 20, panel.Height / 2 - 20);
+            point++;
+            labelPoint.Text = $"Point: {point}";
         }
-
         private void buttonMouse_MouseMove(object sender, MouseEventArgs e)
         {
             Point tmp = new Point(buttonMouse.Location.X + 20, buttonMouse.Location.Y + 20);
@@ -132,30 +65,124 @@ namespace CatchMe
                 ChooseDoor();
             }
         }
-
-        private void Form1_MouseDown(object sender, MouseEventArgs e)
-        {
-            if (e.Button == MouseButtons.Right)
-                contextMenuStrip1.Show(new Point(this.Location.X + e.X, this.Location.Y + e.Y + 20));
-        }
-
+        //
+        // Strip Menu
+        //
         private void StripMenu_Click(object sender, EventArgs e)
         {
-            this.MouseMove -= new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.panel.MouseMove -= new System.Windows.Forms.MouseEventHandler(this.panel_MouseMove);
             this.buttonMouse.MouseMove -= new System.Windows.Forms.MouseEventHandler(this.buttonMouse_MouseMove);
-            timer.Start();
+            timerFreeze.Start();
         }
-
         private void contextMenuStrip1_MouseLeave(object sender, EventArgs e)
         {
-            contextMenuStrip1.Close();
+            contextMenuStrip.Close();
         }
-
+        //
+        // Timer Freeze
+        //
         private void timer_Tick(object sender, EventArgs e)
         {
-            timer.Stop();
-            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            timerFreeze.Stop();
+            this.panel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel_MouseMove);
             this.buttonMouse.MouseMove += new System.Windows.Forms.MouseEventHandler(this.buttonMouse_MouseMove);
+        }
+        //
+        // Timer time
+        //
+        private void timerTime_Tick(object sender, EventArgs e)
+        {
+            time += 1;
+            string tmp = Convert.ToString((double)time / 10);
+            if ((time % 10) == 0)
+                tmp += ".0";            
+            labelTime.Text = tmp + " Time";
+        }
+        //
+        // Panel game field
+        //
+        private void panel_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point tmp = new Point(buttonMouse.Location.X + 20, buttonMouse.Location.Y + 20);
+            int distance = Distanse(tmp, e.Location);
+            if (distance < 50)
+            {
+                tmp.X += 10;
+                if (tmp.X <= panel.Width - 20 && Distanse(tmp, e.Location) >= 50)
+                {
+                    buttonMouse.Location = new Point(tmp.X - 20, tmp.Y - 20);
+                    return;
+                }
+                tmp.X -= 20;
+                if (tmp.X >= 30 && Distanse(tmp, e.Location) >= 50)
+                {
+                    buttonMouse.Location = new Point(tmp.X - 20, tmp.Y - 20);
+                    return;
+                }
+                tmp.X += 10;
+                tmp.Y += 10;
+                if (tmp.Y <= panel.Height - 30 && Distanse(tmp, e.Location) >= 50)
+                {
+                    buttonMouse.Location = new Point(tmp.X - 20, tmp.Y - 20);
+                    return;
+                }
+                tmp.Y -= 20;
+                if (tmp.Y >= 30 && Distanse(tmp, e.Location) >= 50)
+                {
+                    buttonMouse.Location = new Point(tmp.X - 20, tmp.Y - 20);
+                    return;
+                }
+            }
+            if (distance < 50 && IsHouse())
+            {
+                ChooseDoor();
+            }
+        }
+        private void panel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+                contextMenuStrip.Show(new Point(this.Location.X + e.X, this.Location.Y + e.Y + 50));
+        }
+        //
+        // Support methods
+        //
+        private void ChooseDoor()
+        {
+            int door = MyRand.GetRand(1, 4);
+            switch (door)
+            {
+                case 1:
+                    buttonMouse.Location = new Point(20, 20);
+                    break;
+                case 2:
+                    buttonMouse.Location = new Point(panel.Width - 50, 20);
+                    break;
+                case 3:
+                    buttonMouse.Location = new Point(20, panel.Height - 50);
+                    break;
+                case 4:
+                    buttonMouse.Location = new Point(panel.Width - 50, panel.Height - 50);
+                    break;
+                default:
+                    break;
+            }
+        }
+        private bool IsHouse()
+        {
+            if (buttonMouse.Location.X < 40 && buttonMouse.Location.Y < 40)
+                return true;
+            if (buttonMouse.Location.X + 40 > panel.Width - 50 && buttonMouse.Location.Y < 40)
+                return true;
+            if (buttonMouse.Location.X < 40 && buttonMouse.Location.Y + 40 > panel.Height - 70)
+                return true;
+            if (buttonMouse.Location.X + 40 > panel.Width - 50 && buttonMouse.Location.Y + 40 > panel.Height - 70)
+                return true;
+            return false;
+
+        }
+        private int Distanse(Point a, Point b)
+        {
+            return (int)Math.Sqrt(Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y - b.Y, 2));
         }
     }
 }
